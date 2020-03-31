ebbe559e37fbbf2680cf45098a40fa29ed20bb53
Jorge Eduardo <jeduoliveira81@gmail.com>
2020-03-31 12:57:33 -0300

    add metricbeat

--- lumisxp-single-monitoring/docker/lumisxp/setenv.sh
+++ lumisxp-single-monitoring/docker/lumisxp/setenv.sh
@@ -3,6 +3,6 @@ JAVA_OPTS="-Dfile.encoding=UTF8"
 CATALINA_OPTS="$CATALINA_OPTS -XmsTOMCAT_HEAP -XmxTOMCAT_HEAP "
 
 CATALINA_OPTS="$CATALINA_OPTS  -javaagent:/usr/local/elastic-apm-agent-1.7.0.jar \
-     -Delastic.apm.server_url=http://beat:8200 \
+     -Delastic.apm.server_url=http://apm-server:8200 \
      -Delastic.apm.service_name=lumisportal \
      -Delastic.apm.application_packages=lumis"
